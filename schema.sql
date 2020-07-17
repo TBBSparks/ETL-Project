2a983b229b8dac071fbe01b27fcac975c6c2a5cf
sarah-martin <smartin@globaltieskc.org>
2020-07-16 21:31:00 -0500

    Began data cleaning

---
+++ schema.sql
@@ -0,0 +1,69 @@
+CREATE TABLE crime_2014 (
+id INT PRIMARY KEY,
+zip INT,
+description TEXT,
+age INT,
+sex TEXT,
+race TEXT
+);
+
+CREATE TABLE crime_2015 (
+id INT PRIMARY KEY,
+zip INT,
+description TEXT,
+age INT,
+sex TEXT,
+race TEXT
+);
+
+CREATE TABLE crime_2016 (
+id INT PRIMARY KEY,
+zip INT,
+description TEXT,
+age INT,
+sex TEXT,
+race TEXT
+);
+
+CREATE TABLE crime_2017 (
+id INT PRIMARY KEY,
+zip INT,
+description TEXT,
+age INT,
+sex TEXT,
+race TEXT
+);
+
+CREATE TABLE crime_2018 (
+id INT PRIMARY KEY,
+zip INT,
+description TEXT,
+age INT,
+sex TEXT,
+race TEXT
+);
+
+CREATE TABLE crime_2019 (
+id INT PRIMARY KEY,
+zip INT,
+description TEXT,
+age INT,
+sex TEXT,
+race TEXT
+);
+
+CREATE TABLE crime_2020 (
+id INT PRIMARY KEY,
+zip INT,
+description TEXT,
+age INT,
+sex TEXT,
+race TEXT
+);
+
+CREATE TABLE zipcode_population (
+zip INT PRIMARY KEY,
+population INT,
+density INT
+);
+
